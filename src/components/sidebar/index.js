f24144626d4d1908f43c86d53abae8c2e6753bfe
Ting-Ting Chen <hchunyu.tty@gmail.com>
2022-02-21 20:02:30 +0000

    Mobile responsiveness added

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,14 +4,17 @@ import Suggestions from "./suggestions";
 import LoggedInUserContext from "../../context/logged-in-user";
 
 export default function Sidebar() {
-    const {
-        user: { docId = "", fullName, username, userId, following } = {}
-    } = useContext(LoggedInUserContext);
-    
-    return(
-        <div className="p-4">
-            <User username={username} fullName={fullName}/>
-            <Suggestions userId={userId} following={following} loggedInUserDocId={docId}/>
-        </div>
-    );
-};
+  const { user: { docId = "", fullName, username, userId, following } = {} } =
+    useContext(LoggedInUserContext);
+
+  return (
+    <div className="hidden lg:block p-4">
+      <User username={username} fullName={fullName} />
+      <Suggestions
+        userId={userId}
+        following={following}
+        loggedInUserDocId={docId}
+      />
+    </div>
+  );
+}
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,21 +4,19 @@ import LoggedInUserContext from "../context/logged-in-user";
 import usePhotos from "../hooks/use-photos";
 import Post from "./post";
 
-
 export default function Timeline() {
-    const { user } = useContext(LoggedInUserContext);
-    const { photos } = usePhotos(user);
+  const { user } = useContext(LoggedInUserContext);
+  const { photos } = usePhotos(user);
 
-    return (
-        <div className="container col-span-2">
-            {!photos ? (
-                <>
-                    <Skeleton count={4} width={640} height={500} className="mb-5"/>
-                    
-                </> 
-            ) : (
-                photos.map((content) => <Post key={content.docId} content={content} />)
-            )}
-        </div>
-    );
-};
+  return (
+    <div className="col-span-3 lg:col-span-2">
+      {!photos ? (
+        <>
+          <Skeleton count={4} width={640} height={500} className="mb-5" />
+        </>
+      ) : (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      )}
+    </div>
+  );
+}
